40f4ad978d2af440e2a03d97e2fd9a22163e6a4e
ltyap <litoong.yap@gmail.com>
2026-08-12 14:35:25 +0200

    updated postTKEBudgets and postScalarBudgets to be able to execute at specified times now

--- applications/utilities/postScalarBudgets/postScalarBudgets.C
+++ applications/utilities/postScalarBudgets/postScalarBudgets.C
@@ -1,9 +1,9 @@
 /*---------------------------------------------------------------------------*\
 Application
-    postLES
+    postScalarBudgets
 
 Description
-	Calculates second moment statistics based on UMean, together with production and dissipation
+	Calculates variance, cross-terms and dissipation for scalar transport
     
 
 \*---------------------------------------------------------------------------*/
@@ -14,77 +14,94 @@ Description
 
 int main(int argc, char *argv[])
 {
+    argList::addNote
+    (
+        "Calculate scalar budget terms"
+    );
+
+    argList::noParallel();
+    timeSelector::addOptions();
+
+
+
 	#include "setRootCase.H"
 	#include "createTime.H"
-	#include "createMesh.H"
 
+    // Get times list
+    instantList timeDirs = timeSelector::select0(runTime, args);
+
+
+	#include "createMesh.H"
 	#include "readTransportProperties.H"
 	
-    #include "readFields.H"   
-	
-	volScalarField Tp2Mean
-	(
-		IOobject
-        (   
-            "Tp2Mean",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		TTMean - (TMean * TMean)
-	);
-    volVectorField UPrimeTMean
-	(
-		IOobject
-        (   
-            "UPrimeTMean",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		uiTMean - (UMean * TMean)
-	);
-	// volScalarField production
-	// (
-	// 	IOobject
-    //     (   
-    //         "production",
-    //         runTime.timeName(),
-    //         mesh,
-    //         IOobject::NO_READ,
-    //         IOobject::AUTO_WRITE
-    //     ),
-	// 	-(Up2Mean && djuiMean)
-	// );
-	
-	volScalarField scalarDiss
-	(
-		IOobject
-        (   
-            "scalarDiss",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		-alpha*(djTdjTMean - (djTMean & djTMean))
-	);
+    forAll(timeDirs, timeI)
+    {    
+        #include "readFields.H"   
+        
+        volScalarField Tp2Mean
+        (
+            IOobject
+            (   
+                "Tp2Mean",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            TTMean - (TMean * TMean)
+        );
+        volVectorField UPrimeTMean
+        (
+            IOobject
+            (   
+                "UPrimeTMean",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            uiTMean - (UMean * TMean)
+        );
+        // volScalarField production
+        // (
+        // 	IOobject
+        //     (   
+        //         "production",
+        //         runTime.timeName(),
+        //         mesh,
+        //         IOobject::NO_READ,
+        //         IOobject::AUTO_WRITE
+        //     ),
+        // 	-(Up2Mean && djuiMean)
+        // );
+        
+        volScalarField scalarDiss
+        (
+            IOobject
+            (   
+                "scalarDiss",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            -alpha*(djTdjTMean - (djTMean & djTMean))
+        );
 
-	
-    Info<< "Writing Tp2Mean to " << runTime.timeName() << endl;
- 	Tp2Mean.write();
+        
+        Info<< "Writing Tp2Mean to " << runTime.timeName() << endl;
+        Tp2Mean.write();
 
-    Info<< "Writing UPrimeTMean to " << runTime.timeName() << endl;
- 	UPrimeTMean.write();
+        Info<< "Writing UPrimeTMean to " << runTime.timeName() << endl;
+        UPrimeTMean.write();
 
-    Info<< "Writing scalarDiss to " << runTime.timeName() << endl;
- 	scalarDiss.write();
+        Info<< "Writing scalarDiss to " << runTime.timeName() << endl;
+        scalarDiss.write();
 
 
 
-    Info<< endl;
+        Info<< endl;
+    }
     
 
     return(0);
--- applications/utilities/postTKEBudgets/postTKEBudgets.C
+++ applications/utilities/postTKEBudgets/postTKEBudgets.C
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------*\
 Application
-    postLES
+    postTKEBudgets
 
 Description
 	Calculates second moment statistics based on UMean, together with production and dissipation
@@ -14,108 +14,123 @@ Description
 
 int main(int argc, char *argv[])
 {
+    argList::addNote
+    (
+        "Calculate full set of TKE budget terms"
+    );
+
+    argList::noParallel();
+    timeSelector::addOptions();
+
 	#include "setRootCase.H"
 	#include "createTime.H"
-	#include "createMesh.H"
 
+    // Get times list
+    instantList timeDirs = timeSelector::select0(runTime, args);
+
+	#include "createMesh.H"
 	#include "readTransportProperties.H"
 	
-    #include "readFields.H"   
-	
-	volSymmTensorField Up2Mean
-	(
-		IOobject
-        (   
-            "Up2Mean",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		uiujMean - symm(UMean * UMean)
-	);
-	
-	volScalarField production
-	(
-		IOobject
-        (   
-            "production",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		-(Up2Mean && djuiMean)
-	);
-	
-	volScalarField dissipation
-	(
-		IOobject
-        (   
-            "dissipation",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		-nu*(djuidjuiMean - (djuiMean && djuiMean))
-	);
+    forAll(timeDirs, timeI)
+    {
+        #include "readFields.H"   
+            
+        volSymmTensorField Up2Mean
+        (
+            IOobject
+            (   
+                "Up2Mean",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            uiujMean - symm(UMean * UMean)
+        );
+        
+        volScalarField production
+        (
+            IOobject
+            (   
+                "production",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            -(Up2Mean && djuiMean)
+        );
+        
+        volScalarField dissipation
+        (
+            IOobject
+            (   
+                "dissipation",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            -nu*(djuidjuiMean - (djuiMean && djuiMean))
+        );
 
-	volScalarField diff
-	(
-		IOobject
-        (   
-            "diff",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		nu*(uiLuiMean - (UMean & LuiMean))
-	);
+        volScalarField diff
+        (
+            IOobject
+            (   
+                "diff",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            nu*(uiLuiMean - (UMean & LuiMean))
+        );
 
-	volScalarField pDiff
-	(
-		IOobject
-        (   
-            "pDiff",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		-(uiGcpMean - (UMean & GcpMean))
-	);
+        volScalarField pDiff
+        (
+            IOobject
+            (   
+                "pDiff",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            -(uiGcpMean - (UMean & GcpMean))
+        );
 
-	volScalarField conv
-	(
-		IOobject
-        (   
-            "conv",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-		-(uiCuiMean - (UMean & CuiMean))
-	);
+        volScalarField conv
+        (
+            IOobject
+            (   
+                "conv",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            -(uiCuiMean - (UMean & CuiMean))
+        );
 
-    Info<< "Writing Up2Mean to " << runTime.timeName() << endl;
- 	Up2Mean.write();
-    Info<< "Writing production to " << runTime.timeName() << endl;
- 	production.write();
-    Info<< "Writing dissipation to " << runTime.timeName() << endl;
- 	dissipation.write();
+        Info<< "Writing Up2Mean to " << runTime.timeName() << endl;
+        Up2Mean.write();
+        Info<< "Writing production to " << runTime.timeName() << endl;
+        production.write();
+        Info<< "Writing dissipation to " << runTime.timeName() << endl;
+        dissipation.write();
 
-    Info<< "Writing diff to " << runTime.timeName() << endl;
- 	diff.write();
-    Info<< "Writing pDiff to " << runTime.timeName() << endl;
- 	pDiff.write();
-    Info<< "Writing conv to " << runTime.timeName() << endl;
- 	conv.write();
+        Info<< "Writing diff to " << runTime.timeName() << endl;
+        diff.write();
+        Info<< "Writing pDiff to " << runTime.timeName() << endl;
+        pDiff.write();
+        Info<< "Writing conv to " << runTime.timeName() << endl;
+        conv.write();
 
 
-    Info<< endl;
+        Info<< endl;
+    }
+ 
     
 
     return(0);
